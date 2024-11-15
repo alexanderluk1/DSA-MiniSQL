b694149473850ce63d81c92ce22dce8661666f6a
sara <sara.chen.2023@scis.smu.edu.sg>
2024-11-15 16:49:01 +0800

    QueryToExecute

--- CS201 Project Starter v0.7/src/main/java/edu/smu/smusql/Main.java
+++ CS201 Project Starter v0.7/src/main/java/edu/smu/smusql/Main.java
@@ -9,7 +9,7 @@ public class Main {
      *  Main method for accessing the command line interface of the database engine.
      *  MODIFICATION OF THIS FILE IS NOT RECOMMENDED!
      */
-    static Engine dbEngine = new Engine();
+    public static Engine dbEngine = new Engine();
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
